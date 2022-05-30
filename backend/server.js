c3f1f5f43441c4c2cc7c9af94924b35c1a4eaf81
Zaid Jamil <zeejaydev@gmail.com>
2022-05-30 16:33:04 -0600

    deploy

--- backend/server.js
+++ backend/server.js
@@ -13,16 +13,7 @@ app.use(express.urlencoded({extended:false}));
 app.use(cors())
 
 app.get('/api/allProducts',getProducts)
-// app.post('/api/add',async (req,res)=>{
-//     const prods = await Product.create({
-//         _id:2,
-//         title:'test2',
-//         img:'sfdsaf',
-//         desc:'sdfasdf'
-//     })
-//     console.log(prods)
-//     res.status(200).json(prods)
-// })
+
 
 app.use(express.static(path.join(__dirname,"../client/build")));
 app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,"../","client","build","index.html")));
