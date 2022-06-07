2c5410db7d4e5676638e6460aebccc555a40253f
Zaid Jamil <zeejaydev@gmail.com>
2022-06-07 01:13:26 -0600

    test upload

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -40,11 +40,11 @@ const Admin = () => {
         formData.append("file", file, file.name);
 
         if(file.type === "image/jpeg" || file.type==="image/png" || file.type==="image/jpg"){
-            fetch(`${env}/api/imageUpload`,{
+            fetch(`/api/imageUpload`,{
                 method: 'POST',
                 body: formData,
              }).then(res=>res.json()).then(data=>{
-                setUrl(`${env}/api/uploads/${data.url}`)
+                setUrl(`/api/uploads/${data.url}`)
              })
         }else{
             alert("file must be an image") 
